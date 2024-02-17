4120c8ee2b9a1624ba05df14058f1829ad2e4cad
Arkhip McNutt <ortiz47@mail.ru>
2024-02-17 11:31:59 +0300

    updated lab 6

--- os/lab_6/prog_2.cpp
+++ os/lab_6/prog_2.cpp
@@ -16,6 +16,10 @@ const char* sem_write_name = "/write";
 const char* shared_name = "/shared";
 void* addr = malloc(DATASIZE);
 
+sem_t* sem_write;
+sem_t* sem_read;
+int shared_df;
+
 typedef struct {
     sem_t* writer;
     sem_t* reader;
@@ -42,6 +46,17 @@ void* thread_func(void* args) {
 
 void sig_handler(int signo) {
     printf("\nget SIGNINT; %d\n", signo);
+    printf("Closing semaphores\n");
+    sem_close(sem_write);
+    sem_unlink(sem_write_name);
+
+    sem_close(sem_read);
+    sem_unlink(sem_read_name);
+
+    munmap(addr, DATASIZE);
+    close(shared_df);
+
+    shm_unlink(shared_name);
     exit(0);
 }
 
@@ -52,12 +67,12 @@ int main() {
     signal(SIGINT, sig_handler);
     
     printf("Creating and mapping shared memory\n");
-    int shared_df = shm_open(shared_name, O_CREAT | O_RDWR, S_IRWXU);
+    shared_df = shm_open(shared_name, O_CREAT | O_RDWR, S_IRWXU);
     ftruncate(shared_df, DATASIZE);
     addr = mmap(addr, DATASIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shared_df, 0);
     
-    sem_t* sem_write = sem_open(sem_write_name, O_CREAT, S_IWUSR | S_IRUSR, 1);
-    sem_t* sem_read = sem_open(sem_read_name, O_CREAT, S_IWUSR | S_IRUSR, 1);
+    sem_write = sem_open(sem_write_name, O_CREAT, S_IWUSR | S_IRUSR, 0);
+    sem_read = sem_open(sem_read_name, O_CREAT, S_IWUSR | S_IRUSR, 0);
 
     thread_data args;
     args.writer = sem_write;
